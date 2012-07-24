5a382de26bb1d39520c7d000f2291d9cf184b832
dotob <sk@dotob.de>
2012-07-24 20:36:36 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Satt::Application.config.secret_token = 'dbafec20c17c04d16dbb9a5893dc7445fb4cd9e388602f19c977a53e73c7513f6978e1478b2c9b5622ff8b2047350643862d1338ce34f52db8bfb09365f03cd1'
