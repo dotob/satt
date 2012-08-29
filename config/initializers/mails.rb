f4434e3d7a5ddfb75bbf9b6d6d35b456a530dcf8
dotob <sk@dotob.de>
2012-08-30 00:16:36 +0200

    fix mail setup

--- config/initializers/mails.rb
+++ config/initializers/mails.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['app6607949@heroku.com'],
-  :password       => ENV['9ronpmfb'],
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
   :domain         => 'heroku.com'
 }
 ActionMailer::Base.delivery_method = :smtp
