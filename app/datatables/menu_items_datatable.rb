7daf994cb56a78c3e8eae6d32e392af8aa1421e0
sebastian krämer <sk@dotob.de>
2013-02-15 10:38:09 +0000

    userordersview: disabled add orderitem buttoln when master order is closed

--- app/datatables/menu_items_datatable.rb
+++ app/datatables/menu_items_datatable.rb
@@ -31,7 +31,11 @@ private
   end
 
   def form_for_menu_item_add(menu_item)
-    "<a class=\"btn btn-success add\" href=\"/add_orderitem/#{@user_order.id}/#{menu_item.id}\"><i class=\"icon-plus icon-white\"></i></a>"    
+    if @user_order.master_order.deadline_crossed
+				"<a class=\"btn btn-success disabled add\" href=\"#\"><i class=\"icon-plus icon-white\"></i></a>"    
+		else
+	"<a class=\"btn btn-success add\" href=\"/add_orderitem/#{@user_order.id}/#{menu_item.id}\"><i class=\"icon-plus icon-white\"></i></a>"    
+		end
   end
 
   def menu_items
